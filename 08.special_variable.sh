82a29006ad0e05a6ecbc3a66888a3e5bbaffb8fe
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2026-08-30 22:53:24 +0530

    Adding more variables

--- 08.special_variable.sh
+++ 08.special_variable.sh
@@ -31,5 +31,23 @@ echo "get the process id of the last background command: $!"
     ##6.Error handling
 
     #From next file will start with Data Types.
-    
+    ## Data type:- 
+        ##Name:- "Table"
+        ##Integer:- 12
+        ##Decimal:- 12.1
+        ##Complex_Number:- 2+5i
+        ##String:- "Hello World"
+        ##Log:- log34
+        ##boolean:- true/false
+        ##character:- a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
+        ##float:- 12.34
+        ##long:- 12345678901234567890
+        ##double:- 12.345678901234567890
+        ##int:- i=0, j=1, k=2, l=3, m=4, n=5, o=6, p=7, q=8, r=9, s=10
+        ##there are no explicit data types in bash/shell scripting, 
+            ##all variables are treated as strings by default. 
+            ##However, you can perform arithmetic operations on variables that contain numeric values.
+            
+
+
 
---
+++ 09.data_types.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+## In shell scripting every variable is treated as a string by default.
+
+NUMBER1="1050"
+NUMBER2="2025"
+NAME="Santhosh"
+NAME2="Hemanth"
+
+SUM=$((${NUMBER1}+${NUMBER2}))
+echo "The Sum is: $SUM"
+
+SUM=$((${NUMBER1}+${NUMBER2}+${NAME}))
+echo "The Sum is: $SUM"
+
+SUB=$((NUMBER1-NUMBER2))
+echo "the Subtraction is: $SUB"
+
+SUB=$((NUMBER1-NUMBER2-NAME))
+echo "the Subtraction is: $SUB"
+
+MUL=$((NUMBER1*NUMBER2*NAME))
+echo "the Multiplication is: $MUL"
+
+## Array types
+## In Course we have 6 courses, so we can store them in an array.
+## size is 6, Maximum index is 5, Minimum index is 0
+COURSE=("DevOps" "AWS" "Azure" "GCP" "Docker" "Kubernetes")
+echo "The Course is: ${COURSE[0]}"
+echo "The Course is: ${COURSE[1]}"
+
+## In Course we have 5 courses, so we can store them in an array.
+## size is 5, Maximum index is 4, Minimum index is 0
+SUBJECTS=("Maths" "Science" "Social" "English" "Telugu")
+echo "The Subjects are: ${SUBJECTS[0]}"
+echo "The Subjects are: ${SUBJECTS[1]}"     
+echo "The Subjects are: ${SUBJECTS[4]}"
