47593b388f0ed993164a505cdba0d8cf470d90bd
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2026-08-30 18:04:01 +0530

    Adding Conversation

--- 03.variables.sh
+++ 03.variables.sh
@@ -2,15 +2,15 @@ PERSON1=HEMANTH
 PERSON2=SANTHOSH
 
 
-echo "$$PERSON1" :: Hi ra $PERSON2, how are you doing?
-echo "$$PERSON2" :: I'm doing great, thanks for asking! How about you?
-echo "$$PERSON1" :: I'm good too. Have you been working on any interesting projects lately?
-echo "$$PERSON2" :: Yes, I've been working on a new web application that uses
-echo "$$PERSON1" :: That sounds exciting! What technologies are you using for the web application?
-echo "$$PERSON2" :: I'm using React for the frontend and Node.js for the backend
-echo "$$PERSON1" :: Nice! React and Node.js are a great combination. How
-echo "$$PERSON2" :: I'm also using MongoDB as the database and Express.js as the web framework
-echo "$$PERSON1" :: That's a solid tech stack. Are you facing any challenges with the development process?
-echo "$$PERSON2" :: Yes, I'm having some issues with state management in React. I'm trying to figure out the best way to manage the state of my application
-echo "$$PERSON1" :: I see. State management can be tricky in React. Have you considered using a state management library like Redux or MobX?
-echo "$$PERSON2" :: Yes, I've looked into Redux, but I'm not sure if it's the right fit for my application.
+echo "$PERSON1" :: Hi ra $PERSON2, how are you doing?
+echo "$PERSON2" :: I'm doing great, thanks for asking! How about you?
+echo "$PERSON1" :: I'm good too. Have you been working on any interesting projects lately?
+echo "$PERSON2" :: Yes, I've been working on a new web application that uses
+echo "$PERSON1" :: That sounds exciting! What technologies are you using for the web application?
+echo "$PERSON2" :: I'm using React for the frontend and Node.js for the backend
+echo "$PERSON1" :: Nice! React and Node.js are a great combination. How
+echo "$PERSON2" :: I'm also using MongoDB as the database and Express.js as the web framework
+echo "$PERSON1" :: That's a solid tech stack. Are you facing any challenges with the development process?
+echo "$PERSON2" :: Yes, I'm having some issues with state management in React. I'm trying to figure out the best way to manage the state of my application
+echo "$PERSON1" :: I see. State management can be tricky in React. Have you considered using a state management library like Redux or MobX?
+echo "$PERSON2" :: Yes, I've looked into Redux, but I'm not sure if it's the right fit for my application.
