7cbf533fbf026f1b97acd483420a4450fef14acc
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2026-08-30 18:27:05 +0530

    Adding more variables

--- 07.variables.sh
+++ 07.variables.sh
@@ -4,4 +4,10 @@ day=$(date +%A-%Y-%m-%d-%H-%M-%S)
 echo "Current time stamp: $Date"
 echo "exact date and Day and time with right now: $day"
 
-e
+start_time=$(date +%s)
+
+sleep 10
+
+end_time=$(date +%s)
+
+echo "Script execution time: $((end_time - start_time)) seconds"
