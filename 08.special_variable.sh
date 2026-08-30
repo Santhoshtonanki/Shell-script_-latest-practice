f0747b244bdca8d41dc1db8096d16a400725adfc
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2026-08-30 22:05:59 +0530

    Adding more variables

---
+++ 08.special_variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "All variables passed to the script are stored in a special variable called $@"
+echo "All variables passed to the script are stored in a special variable called $."
+echo "All variables passed to the script are stored in a special variable called $*"
+echo "All variables passed to the script are stored in a special variable called $?"
+echo "All variables passed to the script are stored in a special variable called $$"
+echo "All variables passed to the script are stored in a special variable called $!"
+echo "All variables passed to the script are stored in a special variable called $-"
+echo "All variables passed to the script are stored name in a special variable called $0"
+echo "All variables passed to the script are stored in a special variable called $1"
+echo "All variables passed to the script are stored in a special variable called $2"
