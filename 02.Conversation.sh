a10bcd8b5776b404920142346c7951a38bd662b3
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2026-08-30 17:07:00 +0530

    main

---
+++ 02.Conversation.sh
@@ -0,0 +1,12 @@
+echo "santhosh" :: Hi ra hemanth, how are you doing?
+echo "hemanth" :: I'm doing great, thanks for asking! How about you?
+echo "santhosh" :: I'm good too. Have you been working on any interesting projects lately?
+echo "hemanth" :: Yes, I've been working on a new web application that uses
+echo "santhosh" :: That sounds exciting! What technologies are you using for the web application?
+echo "hemanth" :: I'm using React for the frontend and Node.js for the backend
+echo "santhosh" :: Nice! React and Node.js are a great combination. How
+echo "hemanth" :: I'm also using MongoDB as the database and Express.js as the web framework
+echo "santhosh" :: That's a solid tech stack. Are you facing any challenges with the development process?
+echo "hemanth" :: Yes, I'm having some issues with state management in React. I'm trying to figure out the best way to manage the state of my application
+echo "santhosh" :: I see. State management can be tricky in React. Have you considered using a state management library like Redux or MobX?
+echo "hemanth" :: Yes, I've looked into Redux, but I'm not sure if it's the right fit for my application.
