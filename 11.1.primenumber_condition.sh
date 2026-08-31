3964ee50ba9d45d38e7f2d0d767d2b08340cb356
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2026-08-31 14:05:12 +0530

    Adding more variables

--- 11.1.primenumber_condition.sh
+++ 11.1.primenumber_condition.sh
@@ -3,7 +3,7 @@ echo please enter a number:
 read NUMBER
 
 if [ $NUMBER % 1 -eq 0 ] && [ $NUMBER % $NUMBER -eq 0 ]; then
-    echo "The Given number, $NUMBER is only divided with 1 and $NUMBER and equal to 0 so this is Prime Number"
+    echo "The Given number is only divided with 1 and $NUMBER and equal to 0 so this is Prime Number"
 else 
-    echo "The Given number, $NUMBER is echo "The Given number, $NUMBER is not only divided with 1 and $NUMBER and equal to 0 so this is not Prime Number"not a Prime Number"
+    echo "The Given number is not only divided with 1 and $NUMBER and equal to 0. So this is not Prime Number"
 fi
