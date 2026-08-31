15f9f91527ae182db543c57cfa8e2f656eae96b2
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2026-08-31 17:09:07 +0530

    Adding more variables

--- 11.conditions.sh
+++ 11.conditions.sh
@@ -14,7 +14,7 @@ read NUMBER
 ##we taking the value, The "user entered read NUMBER. 
 ##But in next line immediately I gave NUMBER="$1", 
 ##so that unfortunately the value is overwritten. 
-##That's why $1 being like empty and result getting wrong.
+##That"s why $1 being like empty and result getting wrong.
 
 ## here we have to remove the line NUMBER="$1" and then it will work fine.
 ## Or we  have to remove the line read NUMBER and then it will work fine.
