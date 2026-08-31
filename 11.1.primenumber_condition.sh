20ea3b4a9bf1eedfac696bf3fe75fb80e88bf5fc
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2026-08-31 14:02:04 +0530

    Adding more variables

--- 11.1.primenumber_condition.sh
+++ 11.1.primenumber_condition.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo please enter a number:
+read NUMBER
 
 if [ $NUMBER % 1 -eq 0 ] && [ $NUMBER % $NUMBER -eq 0 ]; then
     echo "The Given number, $NUMBER is only divided with 1 and $NUMBER and equal to 0 so this is Prime Number"
