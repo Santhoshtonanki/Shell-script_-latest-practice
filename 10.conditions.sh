d97b9b0c6b6b556df3f3a93aee98166e2731ddb0
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2026-08-31 12:40:05 +0530

    Adding more variables

--- 10.conditions.sh
+++ 10.conditions.sh
@@ -2,9 +2,9 @@
 
 NUMBER="$1"
 
-if [ $NUMBER is -lt 10]; then
+if [ $NUMBER is -lt 10 ]; then
     echo "The Given Number is less than 10"
-elif [ $NUMBER is -gt 10]; then
+elif [ $NUMBER is -gt 10 ]; then
     echo "Then Given Number is Greater than 10"
 else 
     echo "The Given Number is equal to 10"
