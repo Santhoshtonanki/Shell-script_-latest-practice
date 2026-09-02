40c2a76b8c0507715f8c9a23cc1ffbe2ce46ea61
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2026-09-02 09:21:45 +0530

    Adding more variables

--- Shell-Roboshop/roboshop.sh
+++ Shell-Roboshop/roboshop.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 AMI_ID="ami-0220d79f3f480ecf5"
-SECURITY_GROUP_ID="sg-055c71bb814fafb35"
+SECURITY_GROUP="sg-0fc6e3e381d06a2a8"
 HOSTED_ZONE_ID="Z068858712AH0PK53FI2K"
 DOMAIN_NAME="lylbwof.shop"
 
-for instance in "$@"
-do
+for instance in $@
+do 
     aws ec2 run-instances \
     --image-id "$AMI_ID" \
     --instance-type t3.micro \
-    --security-group-ids "$SECURITY_GROUP_ID" \
-    --count 1 \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --security-group-ids "$SECURITY_GROUP" \
+    --subnet-id subnet-xxxxxxxxxxxxxxxxx \
     --query "Instances[0].InstanceId" \
     --output text
+
 done
