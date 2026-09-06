89bbee7d1b35ffab66c1b7e8edb156d763acd4b4
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2026-09-06 17:29:09 +0530

    Adding more variables

--- Shell-Roboshop/frontend.sh
+++ Shell-Roboshop/frontend.sh
@@ -67,6 +67,7 @@ VALIDATE() {
     systemctl restart nginx &>>"$LOG_FILE"
     VALIDATE $? "restarting nginx"
 
-    END_TIME="($date +%s)"
+    END_TIME="$(date +%s)"
     TOTAL_TIME="$(("$END_TIME" - "$START_TIME"))" | tea -a "$LOG_FILE"
+
     echo "total executed time for installing "$TOTAL_TIME"
