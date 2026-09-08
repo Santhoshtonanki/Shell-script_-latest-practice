0959d4692dea5a393f6286ca2af34627c581ae76
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2026-09-08 14:11:24 +0530

    Adding more variables

--- Shell-script/18.loop.sh
+++ Shell-script/18.loop.sh
@@ -13,7 +13,7 @@ SCRIPT_FILE=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_FILE.log"
 
 mkdir -p $LOGS_FOLDER 
-echo -e "$G...SCRIPT STARTED....$N Executed at: $(date)" | tee -a $LOG_FILE
+echo -e "$G...SCRIPT STARTED....$N Executed at: $(date)" | tee -a "$LOG_FILE"
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R...PLEASE RUN THIS SCRIPT WITH ROOT PRIVILEGES...$N"
@@ -22,20 +22,20 @@ fi
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then
-        echo -e "$R...FAILURE....$N.....:: $2 Installation process got $R....Failure...$N" | tee -a $LOG_FILE
+        echo -e "$R...FAILURE....$N.....:: $2 Installation process got $R....Failure...$N" | tee -a "$LOG_FILE"
         exit 1
     else 
-        echo -e "$G...SUCCESS....$N.....:: $2 Installation process got $G....Success...$N" | tee -a $LOG_FILE
+        echo -e "$G...SUCCESS....$N.....:: $2 Installation process got $G....Success...$N" | tee -a "$LOG_FILE"
     fi
 }
 
 for package in "$@" # we should not use $ before package.
 do
-    dnf list installed $package &>>$LOG_FILE
+    dnf list installed $package &>>"$LOG_FILE"
     if [ $? -ne 0 ]; then
-        dnf install $package -y &>>$LOG_FILE
+        dnf install $package -y &>>"$LOG_FILE"
         VALIDATE $? "$package"
     else
-        echo -e "$package is already installed,$Y...........skipping.....$N the installation process" | tee -a $LOG_FILE
+        echo -e "$package is already installed,$Y...........skipping.....$N the installation process" | tee -a "$LOG_FILE"
     fi
 done
